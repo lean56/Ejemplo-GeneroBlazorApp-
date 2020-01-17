27451754304aa126afb049655ba6c4c24f4593e6
leonardo emil <theflaco56@hotmail.com>
2020-01-17 14:52:08 -0400

    .

---
+++ GeneroBlazorApp/GeneroBlazorApp/Data/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneroBlazorApp.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Genero> Generos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=EMIL56\SQLEXPRESS; Database =TestDb;Trusted_Connection =true");
+        }
+
+    }
+
+
+
+    public class Genero
+    {
+        [Key]
+        public int GeneroId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
